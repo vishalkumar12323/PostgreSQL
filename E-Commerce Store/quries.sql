99abf0c009be09ca66c4ad42309a016ade8a0c8a
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-07-06 22:44:16 +0530

    added quries.sql.

---
+++ E-Commerce Store/quries.sql
@@ -0,0 +1,5 @@
+-- Returns all customers from Delhi.
+SELECT * FROM public.customers WHERE city = 'Delhi';
+
+-- Returns all products in Ascending order
+SELECT * FROM public.products ORDER BY price;
