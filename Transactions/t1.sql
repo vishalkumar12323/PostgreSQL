73f99f34cac4703d89cef36f7b76556acdc0d988
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-07-09 16:52:01 +0530

    add postgresql transactions

---
+++ Transactions/t1.sql
@@ -0,0 +1,3 @@
+select version() as pgAdmin_version;
+-- A database transaction is a single unit of work that execute a series of operations (ex. reading or updating data)
+-- as an "all or nothing" block.
