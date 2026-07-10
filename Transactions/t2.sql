8bb448b4452a008194c217760e00238962d631fd
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-07-10 23:06:44 +0530

    update with bank acc. example

--- Transactions/t2.sql
+++ Transactions/t2.sql
@@ -1,3 +1,43 @@
 select version();
 
 SELECT * FROM public.accounts;
+
+
+-- Transaction 101
+begin;
+	insert into accounts (account_id, customer_name, balance)
+	values (3, 'Adam', 200.00);
+commit;
+
+select xmin, xmax, account_id, customer_name, balance from public.accounts;
+
+begin;
+	update accounts set balance = balance + 100.00
+	where account_id = 3;
+
+	select * from accounts
+	where account_id = 3;
+
+	delete from accounts
+	where account_id = 3;
+
+commit;
+
+
+begin;
+	update accounts set balance = balance - 50.00
+	where account_id = 1;
+
+	select * from accounts
+	where account_id = 1;
+
+commit;
+
+
+
+
+
+
+
+
+	
